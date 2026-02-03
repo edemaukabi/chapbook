71130b6ea3129020807fd12663f2179c551903e4
edemaukabi <edemaukabi@gmail.com>
2026-02-03 13:13:53 +0100

    chore: setup basic django project

---
+++ authors_api/settings/local.py
@@ -0,0 +1,10 @@
+from .base import * #noqa
+from .base import env
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="V-NBBD5xYaMjC7Gt3XWmdjS5Txtex-eUoBrJ5GJ1UPUHRWiYXi8",)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
