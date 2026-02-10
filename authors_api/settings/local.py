fc25d2b81ca94420ffe59727962d7e8f5aa48401
edemaukabi <edemaukabi@gmail.com>
2026-02-10 19:26:22 +0100

    feat: configure celery, redis and flower

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -8,3 +8,10 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="V-NBBD5xYaMjC7Gt3XWmdjS5Txtex-eUo
 DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "edemacode@gmail.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Haven"
